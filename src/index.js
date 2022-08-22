3e5657b30904eda857bef9350627d4cf642a12a8
Nicka <nickgabriadze@gmail.com>
2022-08-22 11:18:20 +0400

    removed unnecessary icons

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import AddPosts from './components/addPosts.jsx';
 import { Store } from './store.js';
 import { Provider } from 'react-redux';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import SinglePost from './components/singlePost';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -15,7 +15,6 @@ root.render(
         <Routes>
           <Route path='/' element={<MainPage />} />
           <Route path='/post'  element={<AddPosts />}/>
-          <Route path='/edit' element={<SinglePost />}/>
         </Routes>
       </Provider>
     </BrowserRouter>
